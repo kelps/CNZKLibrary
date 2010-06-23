22b627a37018fa5892c93b712ff321705fd3fbb6
Kelps Leite de Sousa <kelps@users.noreply.github.com>
2010-06-23 17:04:32 +0000

    Added the StaysFullScreenWhenUnfocused property to the ToggleFullScreenAction. Added a FullScreen button to the test app.

--- SourceCode/Silverlight/Cnzk.Library.Interactivity/ToggleFullScreenAction.cs
+++ SourceCode/Silverlight/Cnzk.Library.Interactivity/ToggleFullScreenAction.cs
@@ -10,7 +10,7 @@ namespace Cnzk.Library.Interactivity {
         }
 
         protected override void Invoke(object parameter) {
-            Application.Current.Host.Content.IsFullScreen = !Application.Current.Host.Content.IsFullScreen;
+            IsFullScreen = !Application.Current.Host.Content.IsFullScreen;
         }
 
         void Content_FullScreenChanged(object sender, EventArgs e) {
@@ -31,6 +31,24 @@ namespace Cnzk.Library.Interactivity {
             if (nv != Application.Current.Host.Content.IsFullScreen) {
                 Application.Current.Host.Content.IsFullScreen = nv;
             }
+        }
+        #endregion
+
+        #region DependencyProperty StaysFullScreenWhenUnfocused
+        public bool StaysFullScreenWhenUnfocused {
+            get { return (bool)GetValue(StaysFullScreenWhenUnfocusedProperty); }
+            set { SetValue(StaysFullScreenWhenUnfocusedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StayFullScreenWhenUnfocused.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StaysFullScreenWhenUnfocusedProperty =
+            DependencyProperty.Register("StaysFullScreenWhenUnfocused", typeof(bool), typeof(ToggleFullScreenAction), new PropertyMetadata(false, OnStaysFullScreenWhenUnfocusedChanged));
+
+        private static void OnStaysFullScreenWhenUnfocusedChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) {
+            bool newValue = (bool)e.NewValue;
+            Application.Current.Host.Content.FullScreenOptions = newValue ? 
+                System.Windows.Interop.FullScreenOptions.StaysFullScreenWhenUnfocused : 
+                System.Windows.Interop.FullScreenOptions.None;
         }
         #endregion
 
