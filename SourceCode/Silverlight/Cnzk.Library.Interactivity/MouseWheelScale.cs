22a599cc6a60d4ec0c58f442586b02eb443e134f
kelps_cp <SND\kelps_cp>
2010-07-23 11:40:08 +0000

    Added the RelayCommandAction and the first draft of MouseWheelScale. RelayCommandAction is an action behavior that runs an ICommand, but it is capable of passing the eventargs forward to the command, if configured to do so. MouseWheelScale will be a behavior handles scale manipulation of UIElements to simulate experiences similar to Bing or Google maps, but in regular Silverlight controls.

---
+++ SourceCode/Silverlight/Cnzk.Library.Interactivity/MouseWheelScale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Cnzk.Library.Interactivity {
+    public class MouseWheelScale : Behavior<UIElement> {
+
+        private CompositeTransform Transform { get; set; }
+
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            Transform = new CompositeTransform();
+            this.AssociatedObject.RenderTransform = Transform;
+            this.AssociatedObject.MouseWheel += new System.Windows.Input.MouseWheelEventHandler(AssociatedObject_MouseWheel);
+        }
+
+        protected override void OnDetaching() {
+            base.OnDetaching();
+        }
+
+        void AssociatedObject_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e) {
+            double x = e.Delta / 100.0;
+
+        }
+
+    }
+}
