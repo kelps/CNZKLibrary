0c35f73c355f2cc7fee603913829b9abdc4d439a
kelps_cp <SND\kelps_cp>
2011-07-29 22:23:41 +0000

    ClickCountTrigger

---
+++ SourceCode/Silverlight/Cnzk.Library.Interactivity/ClickCountTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Interactivity;
+using System.Windows;
+using System;
+using System.Windows.Input;
+
+namespace Cnzk.Library.Interactivity {
+    public class ClickCountTrigger : TriggerBase<UIElement> {
+        DateTime lastClick;
+        int count = 0;
+        int ClickInterval = 400;
+
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.AddHandler(UIElement.MouseLeftButtonUpEvent, new MouseButtonEventHandler(AssociatedObject_MouseLeftButtonUp), true);
+        }
+
+        void AssociatedObject_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            var now = DateTime.Now;
+            if (now.Subtract(lastClick).TotalMilliseconds < ClickInterval) {
+                count++;
+                lastClick = now;
+                if (count >= ClickCount) {
+                    count = 0;
+                    InvokeActions(e);
+                }
+            } else {
+                count = 1;
+                lastClick = now;
+            }
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.MouseLeftButtonUp -= AssociatedObject_MouseLeftButtonUp;
+            base.OnDetaching();
+        }
+
+        #region DependencyProperty ClickCount
+
+        public int ClickCount {
+            get { return (int)GetValue(ClickCountProperty); }
+            set { SetValue(ClickCountProperty, value); }
+        }
+
+        public static readonly DependencyProperty ClickCountProperty =
+            DependencyProperty.Register("ClickCount", typeof(int), typeof(ClickCountTrigger), new PropertyMetadata(2));
+
+        #endregion
+
+    }
+}
